78230b1c27416119126b8236c319862f422d999e
Anthony Chen <73210441+a-ch3n@users.noreply.github.com>
2026-08-05 23:59:37 -0500

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     ADMIN_TOKEN: str = "yftest123"
 
     # --- LoopMessage (iMessage) ---
-    LOOP_AUTH_KEY: str = "2LBMdZGOA8J7f9YT-_ApFRDVRGMignlYqDS_NOaWxw4bppK2SuSvrwp4rq8MNb4G"
+    LOOP_AUTH_KEY: str = "yftest123"
     LOOP_SECRET_KEY: str = "2LBMdZGOA8J7f9YT-_ApFRDVRGMignlYqDS_NOaWxw4bppK2SuSvrwp4rq8MNb4G"
     LOOP_SENDER_NAME: str = "mei"
     LOOP_WEBHOOK_SECRET: str = "<any random string you also paste in their dashboard>"
